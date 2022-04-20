015827ad44770f2ae0964e415f41a49f7ac8b51d
hodanyusuff <98726595+hodanyusuff@users.noreply.github.com>
2022-04-20 05:15:24 -0600

    Add files via upload

---
+++ movieReducer.js
@@ -0,0 +1,16 @@
+import constants from '../constants/actionTypes';
+
+let initialState = {
+
+}
+
+export default (state = initialState, action) => {
+    let updated = Object.assign({}, state);
+
+    switch (action.type) {
+
+        default:
+            return state;
+
+    }
+}
