736ee72c2b3811132e5781a1fe94a6aae194f08f
hodanyusuff <98726595+hodanyusuff@users.noreply.github.com>
2022-04-20 17:01:30 -0600

    Add files via upload

--- movieReducer.js
+++ movieReducer.js
@@ -1,13 +1,27 @@
 import constants from '../constants/actionTypes';
 
-let initialState = {
+var initialState = {
+    movies: [],
+    selectedMovie: null
 
 }
 
 export default (state = initialState, action) => {
-    let updated = Object.assign({}, state);
+    var updated = Object.assign({}, state);
 
     switch (action.type) {
+        case constants.FETCH_MOVIES:
+            updated['movies'] = action.movies;
+            updated['selectedMovie'] = action.movies[0];
+            return updated;
+
+        case constants.SET_MOVIE:
+            updated['selectedMovie'] = action.selectedMovie;
+            return updated;
+        case constants.FETCH_MOVIE:
+            updated['selectedMovie'] = action.selectedMovie;
+            return updated;
+
 
         default:
             return state;
